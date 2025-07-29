693e6c035f8212c49408a7b0a8b1a81fc1c94055
perera2001 <nandunperera2001@gmail.com>
2025-07-29 20:06:14 +0530

    dto created

---
+++ pos-app/src/main/java/com/example/pos/app/dto/CustomerDTO.java
@@ -0,0 +1,70 @@
+package com.example.pos.app.dto;
+
+public class CustomerDTO {
+    private int customerId;
+    private String customerName;
+    private String customerAddress;
+    private double customerSalary;
+    private String contactNumber;
+    private String nic;
+
+    public CustomerDTO() {
+    }
+
+    public CustomerDTO(int customerId, String customerName, String customerAddress, double customerSalary, String contactNumber, String nic) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.customerSalary = customerSalary;
+        this.contactNumber = contactNumber;
+        this.nic = nic;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public double getCustomerSalary() {
+        return customerSalary;
+    }
+
+    public void setCustomerSalary(double customerSalary) {
+        this.customerSalary = customerSalary;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+}
