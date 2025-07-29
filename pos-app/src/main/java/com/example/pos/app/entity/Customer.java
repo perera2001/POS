5270b453fb868c896c890d75cd81837bc24d0346
perera2001 <nandunperera2001@gmail.com>
2025-07-29 20:01:41 +0530

    entity update

--- pos-app/src/main/java/com/example/pos/app/entity/Customer.java
+++ pos-app/src/main/java/com/example/pos/app/entity/Customer.java
@@ -1,4 +1,90 @@
 package com.example.pos.app.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @Column(name = "customer_id",length = 45)
+    private int customerId;
+
+    @Column(name="customer_name",length = 100,nullable = false)
+    private String customerName;
+
+    @Column(name="customer_address",length = 255)
+    private String customerAddress;
+
+    @Column(name = "customer_salary")
+    private double customerSalary;
+
+    @Column(name = "contact_numbers" )
+    private String contactNumber;
+
+    @Column(name = "nic")
+    private String nic;
+
+    public Customer() {
+    }
+
+    public Customer(int customerId, String customerName, String customerAddress, double customerSalary, String contactNumber, String nic) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.customerSalary = customerSalary;
+        this.contactNumber = contactNumber;
+        this.nic = nic;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public double getCustomerSalary() {
+        return customerSalary;
+    }
+
+    public void setCustomerSalary(double customerSalary) {
+        this.customerSalary = customerSalary;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
 }
